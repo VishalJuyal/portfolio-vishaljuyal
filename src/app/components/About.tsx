315ae474f3ea87b4d7f85c1746223507f567b323
Vishal Juyal <vishaljuyal237@gmail.com>
2024-09-09 23:11:57 +0530

    About and projects add

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -8,6 +8,9 @@ import finance from "../assets/finance.png";
 function About() {
   return (
     <div className="max-w-[1200px] mx-auto" id="about">
+      <h1 className="text-white text-6xl max-w=[320px] mx-auto font-semibold p-4 mb-4">About
+        <span className="text-orange-400">Me</span>
+        </h1>
       <div className="grid grid-cols-8 gap-6 place-items-center">
         <div className="w-full col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-r from-orange-400 via-purple-700 to-orange-800 opacity-30 animate-gradient-xy"></div>
@@ -25,6 +28,7 @@ function About() {
           </div>
         </div>
         <div className="w-full col-span-3 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-r from-orange-400 via-purple-700 to-orange-800 opacity-30 animate-gradient-xy"></div>
           <div className="flex flex-row p-6">
             <Image src={pc} alt="pc" className="w-auto h-[130px]" />
             <div className="flex flex-col mt-4">
@@ -38,6 +42,7 @@ function About() {
           </div>
         </div>{" "}
         <div className="w-full col-span-3 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-r from-orange-400 via-purple-700 to-orange-800 opacity-30 animate-gradient-xy"></div>
           <div className="flex flex-row p-6">
             <Image src={card} alt="card" className="w-auto h-[130px]" />
             <div className="flex flex-col mt-4">
@@ -50,6 +55,7 @@ function About() {
           </div>
         </div>
         <div className="w-full col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-r from-orange-400 via-purple-700 to-orange-800 opacity-30 animate-gradient-xy"></div>
           <div className="flex flex-row p-6">
             <Image src={finance} alt="finance" className="w-auto h-[130px]" />
             <div className="flex flex-col mt-4">
